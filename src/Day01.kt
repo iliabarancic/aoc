4faf2a9c99332a030f5dfb90fd76d99be41b9423
Ilia Barancic <ilia.barancic@iits-consulting.de>
2023-12-08 21:53:34 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(readInput("Day01")).println()
+    part2(readInput("Day01_2")).println()
 }
+
+fun part1(input: List<String>) = input.sumOf { with(it.toCharArray().filter(Char::isDigit)) { "${first()}${last()}" }.toInt() }
+
+fun part2(input: List<String>): Int {
+    return input.sumOf { getFirstAndLastNumber(it) }
+}
+
+fun getFirstAndLastNumber(line: String): Int {
+    val allNumbers = numbers.keys + numbers.values.map { it.toString() }
+    val first = line.findAnyOf(allNumbers)?.second.toNumber()
+    val second = line.findLastAnyOf(allNumbers)?.second.toNumber()
+    return "$first$second".toInt()
+}
+
+private fun String?.toNumber() = this?.let { if (it.length > 1) numbers[it] else it }
+
+val numbers = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
